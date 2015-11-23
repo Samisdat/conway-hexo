3a59f30af5ac3ac621759d54e385fa6c4269982f
samisdat <bastian.sackermann@gmail.com>
2015-11-23 23:18:19 +0000

    create own dir for non source dependencies

---
+++ grunt/copy.js
@@ -0,0 +1,38 @@
+/*global module */
+
+var copy = {
+    fontawesome: {
+        expand: true,
+        src: 'dependencies/bower_components/font-awesome/fonts/**',
+        dest: 'source/bower_components/font-awesome/fonts/',
+        flatten: true,
+        filter: 'isFile'
+    },
+    bootstrap: {
+        expand: true,
+        src: 'dependencies/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap**',
+        dest: 'source/bower_components/bootstrap/javascripts/',
+        flatten: true,
+        filter: 'isFile'
+    },
+    jquery: {
+        expand: true,
+        src: 'dependencies/bower_components/jquery/dist/jquery.**',
+        dest: 'source/bower_components/jquery/',
+        flatten: true,
+        filter: 'isFile'
+    },
+    highlighjs: {
+        expand: true,
+        src: [
+            'dependencies/bower_components/highlightjs/highlight.pack.min.js',
+            'dependencies/bower_components/highlightjs/styles/github.css' 
+        ],
+        dest: 'source/bower_components/highlightjs/',
+        flatten: true,
+        filter: 'isFile'
+    }    
+ 
+};
+
+module.exports = copy;
